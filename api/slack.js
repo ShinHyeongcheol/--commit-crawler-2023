2cdff6c531f2e84ac1d7b2decb0226d9540795f2
Shin Hyeongcheol <hcshin0717@naver.com>
2023-05-03 14:39:57 +0900

    Update Slack.js API

--- api/slack.js
+++ api/slack.js
@@ -6,19 +6,20 @@ const web = new WebClient(CONFIG.slack_api_token);
 const readCommitHistory = async () => {
   const res = web.conversations.history({ channel: CONFIG.slack_channel_id });
   const history = await res.then((res) => {
-	// console.log(res);
+	//console.log(res);
     let messages = res.messages
       .map((e) => {
         if (e['attachments'] === undefined) return null;
         return {
           ts: e['ts'],
           detail: e['attachments'][0],
+		  text: e['text'],
         };
       })
       // Exclude simple messages (ex. someone joined)
       .filter((e) => e !== null)
       // Exclude pr, creating repo
-      .filter(
+	  .filter(
         (e) =>
           e['detail']['fallback'] !== undefined &&
           e['detail']['text'] !== undefined
@@ -30,10 +31,15 @@ const readCommitHistory = async () => {
           commit_text.indexOf('|')
         );
 		const fallback_name = e['detail']['fallback'];
-		let name_in_fallback = fallback_name.substring(
-			fallback_name.indexOf('[') + 1,
-			fallback_name.indexOf('/')
-		)
+		const commit_in_fallbak = fallback_name.substring(
+			fallback_name.indexOf('commit')
+		);
+		let name_in_fallback = commit_in_fallbak.substring(
+			commit_in_fallbak.indexOf('by') + 3
+		);
+		//console.log(commit_in_fallbak);
+		console.log(name_in_fallback);
+		//console.log(commit_text, fallback_name);
         // const offset = new Date().getTimezoneOffset();
         // const ts = new Date(e['ts'] * 1000 - offset * 60 * 1000);
         const ts = new Date(e['ts'] * 1000);
@@ -44,6 +50,7 @@ const readCommitHistory = async () => {
           timestamp: ts
         };
       });
+	  //console.log(messages);
     return messages;
   });
   return history;
